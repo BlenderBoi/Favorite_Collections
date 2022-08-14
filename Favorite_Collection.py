af06d85b7b9e1bb99b058e6e35b172fcd4f1e12d
user <user@example.com>
2022-08-14 11:09:43 +0800

    add object list

--- Favorite_Collection.py
+++ Favorite_Collection.py
@@ -5,6 +5,20 @@ def update_UI():
         for area in screen.areas:
             area.tag_redraw()
 
+def draw_subpanel(self, boolean, property, label, layout):
+
+    if boolean:
+        ICON = "TRIA_DOWN"
+    else:
+        ICON = "TRIA_RIGHT"
+
+    row = layout.row(align=True)
+    row.alignment = "LEFT"
+    row.prop(self, property, text=label, emboss=False, icon=ICON)
+
+    return boolean
+
+
 class FC_UL_Favorite_Collection_List(bpy.types.UIList):
 
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
@@ -55,6 +69,56 @@ class FC_OT_Select_Collection_Objects(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class FC_OT_Collection_Select_Object(bpy.types.Operator):
+    """Select Object (Shift Click to Add to Selection)"""
+    bl_idname = "favorite_collection.select_this_object"
+    bl_label = "Select This Object"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    operation: bpy.props.StringProperty()
+    object: bpy.props.StringProperty()
+
+    def invoke(self, context, event):
+
+        if self.operation == "SELECT":
+            if event.shift:
+                self.deselect = False
+            else:
+                self.deselect = True
+
+
+        
+        return self.execute(context)
+
+
+
+    def execute(self, context):
+
+        scn = context.scene
+
+        object = scn.objects.get(self.object)
+    
+        if object:
+
+            if self.operation == "SELECT":
+                if self.deselect:
+                    for obj in context.selected_objects:
+                        obj.select_set(False)
+
+
+                object.select_set(True)
+                bpy.context.view_layer.objects.active = object
+
+
+
+        return {'FINISHED'}
+
+
+
+
+
+
+
 class FC_PT_Favorite_Collections(bpy.types.Panel):
     """Your Favorite Collection Panel"""
     bl_label = "Favorite Collection"
@@ -82,6 +146,72 @@ class FC_PT_Favorite_Collections(bpy.types.Panel):
         col.separator()
         col.operator("favorite_collection.list_operator", icon="TRIA_UP", text="").operation = "UP"
         col.operator("favorite_collection.list_operator", icon="TRIA_DOWN", text="").operation = "DOWN"
+
+        
+
+
+        if len(scn.Favorite_Collection_List) > index:
+            box = layout.box()
+            row = box.row(align=True)
+            if draw_subpanel(scn, scn.Favorite_Collection_Show_Objects, "Favorite_Collection_Show_Objects", "Show Objects", row):
+                if scn.Favorite_Collection_Show_Objects:
+
+                    item = scn.Favorite_Collection_List[index]
+                    collection = item.Collection
+                    if collection:
+
+                        if len(collection.objects) > 0:
+                            box.label(text="Filter")
+                            col2 = box.column(align=True)
+                            col2.prop(item, "Filter_name", text="")
+                            col2.prop(item, "Filter_types", text="")
+                            for object in collection.objects:
+
+                                filter_item = True
+                                filter_type = True
+
+
+                                if item.Filter_name == "":
+                                    filter_item = False
+                                else:
+                                    if item.Filter_name.lower() in object.name.lower():
+                                        filter_item = False
+                                    else:
+                                        filter_item = True
+
+
+                                if item.Filter_types == "ALL":
+                                    filter_type= False
+
+                                elif item.Filter_types == object.type:
+                                    filter_type= False 
+
+                                
+
+
+
+                                if not filter_item and not filter_type:
+
+
+                                    row2 = box.row(align=True)
+                                    operation = row2.operator("favorite_collection.select_this_object",icon="OBJECT_DATA", text="")
+                                    operation.object = object.name
+                                    row2.separator()
+                                    operation.operation = "SELECT"
+                                    row2.prop(object, "hide_viewport", text="")
+                                    row2.prop(object, "hide_render", text="")
+                                    row2.prop(object, "hide_select", text="")
+
+                                    row2.label(text=object.name, icon=object.type + "_DATA")
+
+
+                        else:
+                            box.label(text="No Object in Collection", icon="INFO")
+
+
+
+
+
 
 ENUM_list_operation = [("ADD","Add","Add"),("REMOVE","Remove","Remove"),("UP","Up","Up"),("DOWN","Down","Down")]
 
@@ -156,14 +286,42 @@ class FC_OT_List_Operator(bpy.types.Operator):
         update_UI()
         return {'FINISHED'}
 
+
+def ENUM_filter_types(self, context):
+    
+    item_types = set()
+    # items = [("ALL", "All", "All", 0, "RADIOBUT_ON"),]
+    items = [("ALL", "All", "All", "RADIOBUT_ON", 0)]
+
+    if self.Collection:
+        for object in self.Collection.objects:
+            item_types.add(object.type)
+
+    for index, type in enumerate(item_types):
+        # items.append([type, type.capitalize(), type, type + "_DATA", index+1])
+        items.append((type, type.capitalize(), type, type + "_DATA", index+1))
+
+    if len(items) > 0:
+        pass
+    else:
+        items = [("None","None","None")]
+
+
+    
+    return items
+                
+                
+
 class Favorite_Collections_Property(bpy.types.PropertyGroup):
     Collection : bpy.props.PointerProperty(type=bpy.types.Collection)
+    Filter_types: bpy.props.EnumProperty(items=ENUM_filter_types)
+    Filter_name: bpy.props.StringProperty(options={'TEXTEDIT_UPDATE'})
 
 
 
 
 
-classes = [Favorite_Collections_Property, FC_OT_List_Operator, FC_UL_Favorite_Collection_List, FC_PT_Favorite_Collections, FC_OT_Select_Collection_Objects]
+classes = [FC_OT_Collection_Select_Object, Favorite_Collections_Property, FC_OT_List_Operator, FC_UL_Favorite_Collection_List, FC_PT_Favorite_Collections, FC_OT_Select_Collection_Objects]
 
 def register():
     for cls in classes:
@@ -171,6 +329,7 @@ def register():
 
     bpy.types.Scene.Favorite_Collection_List = bpy.props.CollectionProperty(type=Favorite_Collections_Property)
     bpy.types.Scene.Favorite_Collection_List_Index = bpy.props.IntProperty()
+    bpy.types.Scene.Favorite_Collection_Show_Objects = bpy.props.BoolProperty()
 
 def unregister():
     for cls in classes:
@@ -178,6 +337,7 @@ def unregister():
 
     del bpy.types.Scene.Favorite_Collection_List
     del bpy.types.Scene.Favorite_Collection_List_Index
+    del bpy.types.Scene.Favorite_Collection_Show_Objects
 
 if __name__ == "__main__":
     register()
